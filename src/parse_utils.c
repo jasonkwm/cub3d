c0e298401754e0599bc49a6e49997dbb11309126
Jason Koh <jasonkwm13@gmail.com>
2023-09-08 16:47:36 +0800

    feat: add free texture and map function and add utils for parsing

---
+++ src/parse_utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jakoh <jakoh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 14:12:48 by jakoh             #+#    #+#             */
+/*   Updated: 2023/09/08 15:47:31 by jakoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+char	check_first_letter(char   *line, char	*valid_set)
+{
+	char	c;
+	char	*trimmed;
+
+	trimmed = ft_strtrim(line, SPACES);
+	if (!trimmed)
+		return (0);
+	c = trimmed[0];
+	free(trimmed);
+	if (ft_strchr(valid_set, c))
+		return(c);
+	return (0);
+}
+
+int		ft_isspace(int c)
+{
+	if ((c >= 9 && c <= 13) || c == '\n' || c == ' ')
+		return (1);
+	return (0);
+}
+
+int		missing_textures(t_texture *texture)
+{
+	if (!texture->north || !texture->south || !texture->east || !texture->west
+		|| !texture->floor || !texture->ceiling)
+		return (1);
+	return (0);
+}
