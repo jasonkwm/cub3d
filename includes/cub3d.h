af519f8fe88a3319b1eb001aaadcb05e03b91b07
Jason Koh <jasonkwm13@gmail.com>
2023-09-08 11:50:36 +0800

    feat: optimize struct and update header file and makefile

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,12 +6,13 @@
 /*   By: jakoh <jakoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 13:22:54 by jakoh             #+#    #+#             */
-/*   Updated: 2023/09/07 15:12:44 by jakoh            ###   ########.fr       */
+/*   Updated: 2023/09/08 11:48:04 by jakoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+# define SPACES " 	\r\n"
 # include <mlx.h>
 # include <stdio.h>
 # include <unistd.h>
@@ -20,44 +21,43 @@
 # include <fcntl.h>
 # include "libft.h"
 
-typedef struct s_texture {
-    char    *north;
-    char    *south;
-    char    *east;
-    char    *west;
-}   t_texture;
 
-typedef struct s_color {
-    char    *floor;
-    char    *ceiling;
-}   t_color;
-
-typedef struct s_map {
-    char    **map;
-    int     max_height;
-    int     max_width;
-
-}   t_map;
-
-typedef struct s_map_info
+typedef struct s_texture
 {
-    t_texture   texture;
-    t_color     color;
-    t_map		map;
-}   t_map_info;
+	char	*north;
+	char	*south;
+	char	*east;
+	char	*west;
+	char	*floor;
+	char	*ceiling;
+}	t_texture;
+
+typedef struct s_map
+{
+	char	**map;
+	int		max_height;
+	int		max_width;
+
+}	t_map;
 
 typedef struct s_variables
 {
-	void	*mlx;
-	void	*window;
-	t_map_info	map_info;	
+	void		*mlx;
+	void		*window;
+	t_texture	texture;
+	t_map		map;
 }	t_variables;
 
+// inits.c
+void	init_variables(t_variables *variables);
+void	init_texture(t_texture *texture);
+void	init_map(t_map *map);
 
 // exits.c
 void	exit_with_message(char *message, int exit_code);
 
 //parse_map.c
-int parse_map (char *file_name);
+int		parse_map(char *file_name);
+
 
 #endif
